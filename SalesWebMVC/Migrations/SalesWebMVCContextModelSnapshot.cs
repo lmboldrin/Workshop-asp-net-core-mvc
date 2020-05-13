96127443be4595fea556719bdb03ed454225fc78
Lucas Boldrin <lmbsud@gmail.com>
2020-05-13 18:36:15 -0300

    Changing theme

--- SalesWebMVC/Migrations/SalesWebMVCContextModelSnapshot.cs
+++ SalesWebMVC/Migrations/SalesWebMVCContextModelSnapshot.cs
@@ -11,7 +11,7 @@ namespace SalesWebMVC.Migrations
     {
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
-#pragma warning disable 612, 618
+#pragma warning disable 612, 618 
             modelBuilder
                 .HasAnnotation("ProductVersion", "2.1.14-servicing-32113")
                 .HasAnnotation("Relational:MaxIdentifierLength", 64);
